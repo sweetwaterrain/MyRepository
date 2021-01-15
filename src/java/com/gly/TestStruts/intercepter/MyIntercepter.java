7a5b55ba314ce9f8e8d424c2c27743cc75a48d9c
Gly <993214226@qq.com>
2021-01-15 13:21:18 +0800

    2021.1.15 13:21 第二次

--- src/java/com/gly/TestStruts/intercepter/MyIntercepter.java
+++ src/java/com/gly/TestStruts/intercepter/MyIntercepter.java
@@ -9,23 +9,23 @@ import com.sun.xml.bind.v2.runtime.unmarshaller.Intercepter;
 import java.util.Map;
 
 
-//public class MyIntercepter extends MethodFilterInterceptor {
-//
-//	@Override
-//	protected String doIntercept(ActionInvocation invocation) throws Exception {
-//		 /*
-//		    拦截器放行 如果拦截器链没有执行完，则进入下一个拦截器，如果当前
-//		    拦截器是链上的最后一个拦截器，则进入action
-//		 */
-//		String result1 = "to_Login";
-//		System.out.println("myintercepter invoke");
-//
-//		Map<String, Object> session = ActionContext.getContext().getSession();
-//		if(session.containsKey("user") ){
-//			if(session.get("user") != null) {
-//				return invocation.invoke();
-//			}
-//		}
-//		return result1;
-//	}
-//}
+public class MyIntercepter extends MethodFilterInterceptor {
+
+	@Override
+	protected String doIntercept(ActionInvocation invocation) throws Exception {
+		 /*
+		    拦截器放行 如果拦截器链没有执行完，则进入下一个拦截器，如果当前
+		    拦截器是链上的最后一个拦截器，则进入action
+		 */
+		String result1 = "to_Login";
+		System.out.println("myintercepter invoke");
+
+		Map<String, Object> session = ActionContext.getContext().getSession();
+		if(session.containsKey("user") ){
+			if(session.get("user") != null) {
+				return invocation.invoke();
+			}
+		}
+		return result1;
+	}
+}
